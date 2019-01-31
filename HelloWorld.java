16fb4bbb328c44a6f7662cab61dcb74af878a99a
oksanagrn <46922209+oksanagrn@users.noreply.github.com>
2019-01-31 10:07:17 +0200

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class HelloWorld {
+
+    public static void main (String[] args)
+    {
+        System.out.println("Welcome");
+        System.out.print("to our training course!");
+    }
+}
