828861927badb1da4e43f61a843db92e8ae93f46
v.potemskyi <volodum@gmail.com>
2019-02-08 16:04:40 +0200

    test

--- src/test/java/lesson6/Practice/Administrator.java
+++ src/test/java/lesson6/Practice/Administrator.java
@@ -1,10 +1,11 @@
-package lesson6.Practice;
+ package test.java.lesson6.Practice;
 
 public class Administrator extends User {
 
     @Override
     public void login(String userName, String userPass) {
-        //Add code here.
+        numberoflogins++;
+        System.out.print("Login as: "+userName+" , "+userPass+".  ");
         System.out.println("Role: administrator.");
     }
 }
--- src/test/java/lesson6/Practice/Guest.java
+++ src/test/java/lesson6/Practice/Guest.java
@@ -1,15 +1,22 @@
-package lesson6.Practice;
+package test.java.lesson6.Practice;
 
 public class Guest extends User {
 
     @Override
     public void login(String userName, String userPass) {
-        // Add code here.
-        System.out.println("Role: guest.");
+        numberoflogins++;
+        System.out.print("Login as: "+userName+" , "+userPass+".  ");
+    System.out.println("Role: guest.");
     }
-
     public void login(String userName) {
-        // Add code here that check if userName equals "guest" then login with guest/guest credentials,
-        // else - print "Wrong default login name" message.
+
+        if (userName.equals("guest")) {
+            System.out.print("Login as: "+userName+".  ");
+            numberoflogins++;
+            System.out.println("Role: guest.");
+        }
+        else{
+            System.out.println("Wrong default login name");
+        }
     }
 }
